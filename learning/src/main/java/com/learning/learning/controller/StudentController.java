690df028be2ae7f027efd7ef062ee2b8b1970259
Prakriti Maurya <Int202628@kanerika.com>
2026-08-14 15:37:44 +0530

    changes in student controller

--- learning/src/main/java/com/learning/learning/controller/StudentController.java
+++ learning/src/main/java/com/learning/learning/controller/StudentController.java
@@ -16,7 +16,6 @@ import java.util.List;
 @RequestMapping("/api/students")
 public class StudentController {
 
-
     private final StudentService studentService;
 
     public StudentController(StudentService studentService) {
